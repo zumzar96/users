848b342b7b12c239138fc6f66b16e8b6936a8f5d
ismail <ismailgim96@gmail.com>
2021-12-16 12:36:53 +0100

    addin buton

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 import Card from '../UI/Card';
 
-import classes from '../Users/AddUser.module.css'
+import Button from '../UI/Button';
+
+
+import classes from '../Users/AddUser.module.css';
 
 
 
@@ -19,7 +22,7 @@ const AddUser = (props) => {
             <input id="username" type="text" />
             <label htmlFor="age">Age (Years)</label>
             <input id="age" type="number" />
-            <button type="submit">Add User</button>
+            <Button type="submit">Add User</Button>
             </form>
       </Card>  
     );
